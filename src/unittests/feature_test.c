a724540383fc342e1bb9330350d8d0c989567791
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-06-05 19:39:10 +0530

    Simplification of mode_is_dirty handler
    Also includes:
    * Minor change to usart_test_normal to check if output char is
      incremented per iteration.

--- src/unittests/feature_test.c
+++ src/unittests/feature_test.c
@@ -3,20 +3,25 @@
 #define YUKTI_TEST_IMPLEMENTATION
 #include <yukti.h>
 #include <mocks.h>
+#include <assert.h>
 
-bool mode_is_dirty_ofter_once_handler()
+bool mode_is_dirty_after_some_iterations_handler()
 {
-    return mode_is_dirty_fake.invokeCount > 1;
+    assert (mode_is_dirty_fake.resources != NULL);
+    return mode_is_dirty_fake.invokeCount > *((unsigned int*)mode_is_dirty_fake.resources);
 }
 
 YT_TEST (test, usart_test_normal)
 {
-    mode_is_dirty_fake.handler = mode_is_dirty_ofter_once_handler;
+    unsigned int iter_num        = 2;
+    mode_is_dirty_fake.resources = &iter_num;
+    mode_is_dirty_fake.handler   = mode_is_dirty_after_some_iterations_handler;
 
     YT_MUST_CALL_IN_ORDER (usart_on);
     YT_MUST_CALL_IN_ORDER (loop_delay, YT_V (USART_TEST_DELAY_LOOP_COUNT));
     YT_MUST_CALL_IN_ORDER (usart_send_string, _);
     YT_MUST_CALL_IN_ORDER (usart_send_char, YT_V ('A'));
+    YT_MUST_CALL_IN_ORDER (usart_send_char, YT_V ('B'));
     YT_MUST_CALL_IN_ORDER (usart_off);
 
     usart_test();
@@ -28,7 +33,9 @@ YT_TEST (test, usart_test_normal)
 
 YT_TEST (test, holdoff_test_normal)
 {
-    mode_is_dirty_fake.handler = mode_is_dirty_ofter_once_handler;
+    unsigned int iter_num        = 1;
+    mode_is_dirty_fake.resources = &iter_num;
+    mode_is_dirty_fake.handler   = mode_is_dirty_after_some_iterations_handler;
 
     YT_MUST_CALL_IN_ORDER (holdoff_test_init);
     YT_MUST_CALL_IN_ORDER (holdoff_test_body, YT_V (HOLDOFF_TEST_PULSE_WIDTH_LOOP_COUNT));
@@ -43,7 +50,9 @@ YT_TEST (test, holdoff_test_normal)
 
 YT_TEST (test, runt_pulse_test_normal_pulses)
 {
-    mode_is_dirty_fake.handler = mode_is_dirty_ofter_once_handler;
+    unsigned int iter_num        = 1;
+    mode_is_dirty_fake.resources = &iter_num;
+    mode_is_dirty_fake.handler   = mode_is_dirty_after_some_iterations_handler;
 
     YT_MUST_CALL_IN_ORDER (runt_pulse_init);
     YT_MUST_CALL_IN_ORDER (runt_pulse_body, YT_V (RUNT_TEST_NORMAL_PULSE_WIDTH_LOOP_COUNT),
@@ -58,14 +67,11 @@ YT_TEST (test, runt_pulse_test_normal_pulses)
     YT_END();
 }
 
-bool mode_is_dirty_after_one_runt_pulse_handler()
-{
-    return mode_is_dirty_fake.invokeCount > RUNT_PULSE_FREQ;
-}
-
 YT_TEST (test, runt_pulse_test_runt_pulses)
 {
-    mode_is_dirty_fake.handler = mode_is_dirty_after_one_runt_pulse_handler;
+    unsigned int iter_num        = RUNT_PULSE_FREQ;
+    mode_is_dirty_fake.resources = &iter_num;
+    mode_is_dirty_fake.handler   = mode_is_dirty_after_some_iterations_handler;
 
     YT_MUST_CALL_IN_ORDER (runt_pulse_init);
     YT_MUST_CALL_IN_ORDER (runt_pulse_body, YT_V (RUNT_TEST_NORMAL_PULSE_WIDTH_LOOP_COUNT),
