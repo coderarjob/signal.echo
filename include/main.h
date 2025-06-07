2e3097414b38bffdf8a56e5ebe29ea0e38ee4d3c
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-06-08 02:45:03 +0530

    Minor fixes to compiler warning during Unittest build

--- include/main.h
+++ include/main.h
@@ -2,21 +2,21 @@
 
 #include <stdbool.h>
 
-#define USART_TEST_DELAY_LOOP_COUNT             65535
+#define USART_TEST_DELAY_LOOP_COUNT             65535U
 #define USART_TEST_STRING                       "Hello"
 
-#define HOLDOFF_TEST_NUMBER_OF_PULSES           20
-#define HOLDOFF_TEST_PULSE_WIDTH_LOOP_COUNT     255
-#define HOLDOFF_TEST_GAP_DELAY_LOOP_COUNT       65535
+#define HOLDOFF_TEST_NUMBER_OF_PULSES           20U
+#define HOLDOFF_TEST_PULSE_WIDTH_LOOP_COUNT     255U
+#define HOLDOFF_TEST_GAP_DELAY_LOOP_COUNT       65535U
 
 #define RUNT_PULSE_FREQ                         1000 // 1 runt pulse after this many pulses
-#define RUNT_TEST_RUNT_PULSE_WIDTH_LOOP_COUNT   255
-#define RUNT_TEST_NORMAL_PULSE_WIDTH_LOOP_COUNT 512
+#define RUNT_TEST_RUNT_PULSE_WIDTH_LOOP_COUNT   255U
+#define RUNT_TEST_NORMAL_PULSE_WIDTH_LOOP_COUNT 512U
 
-#define RUNT_TEST_RUNT_HIGH_LEVEL               0x7
-#define RUNT_TEST_RUNT_LOW_LEVEL                0x7
-#define RUNT_TEST_NORMAL_HIGH_LEVEL             0xF // 0xF is full rail high
-#define RUNT_TEST_NORMAL_LOW_LEVEL              0x0 // 0x0 is full rail low
+#define RUNT_TEST_RUNT_HIGH_LEVEL               0x7U
+#define RUNT_TEST_RUNT_LOW_LEVEL                0x7U
+#define RUNT_TEST_NORMAL_HIGH_LEVEL             0xFU // 0xF is full rail high
+#define RUNT_TEST_NORMAL_LOW_LEVEL              0x0U // 0x0 is full rail low
 
 typedef enum TestModes {
     UNKNOWN_TEST,
